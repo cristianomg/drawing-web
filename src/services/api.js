cfe0bd0a8dd581c03b9b974db4e714857beb5871
cristiano oliveira <cristiano.oliveira@radixeng.com.br>
2022-06-07 18:57:59 -0300

    alteração de url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://dbe8-2804-1b1-1290-208b-9463-8298-9315-f3a.sa.ngrok.io/api',
+    baseURL: 'https://b6a3-187-87-229-70.sa.ngrok.io/api',
 });
